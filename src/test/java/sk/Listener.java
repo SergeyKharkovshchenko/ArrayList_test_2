5540ef72f658494148eba11c68ec133d7f1bc72a
SergeyKharkovshchenko <kharkovchenko@gmail.com>
2017-07-20 12:32:06 +0300

    added Listener

--- src/test/java/sk/Listener.java
+++ src/test/java/sk/Listener.java
@@ -1,7 +1,20 @@
 package sk;
 
-/**
- * Created by SK on 20.07.2017.
- */
-public class Listener {
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+public class Listener extends TestListenerAdapter {
+
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("Test " + iTestResult.getName() + " success");
+    }
+
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("Test " + iTestResult.getName() +
+                " failed, error " + iTestResult.getThrowable());
+    }
+
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println("Test " + iTestResult.getName() + " skipped");
+    }
 }
